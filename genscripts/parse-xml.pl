c89e69c4bb8a651e933861c0f1d211b0ce0c7a3d
Scott Lanning <slanning@cpan.org>
2016-12-07 19:22:39 +0100

    WIP merging Typedef into Struct

--- genscripts/parse-xml.pl
+++ genscripts/parse-xml.pl
@@ -16,14 +16,45 @@ exit;
 
 sub main {
     my $root = root_node();
-    my @nodes = top_nodes_from_header_file($root);
-    process_top_nodes(\@nodes);
+
+    my @top_nodes = top_nodes_from_header_file($root);
+    my $nodes_by_id = nodes_by_id($root);
+
+    process_top_nodes(\@top_nodes, $nodes_by_id);
 }
 
 sub root_node {
     my $doc = XML::LibXML->load_xml(IO => *STDIN);
     my $root = $doc->documentElement();
     return($root);
+}
+
+sub nodes_by_id {
+    my ($node) = @_;
+    my @id_nodes = nodes_with_id($node);
+
+    my %nodes_by_id;
+    foreach my $id_node (@id_nodes) {
+        my $id = $id_node->getAttribute('id');
+        $nodes_by_id{$id} = $id_node;
+    }
+    return \%nodes_by_id;
+}
+
+sub nodes_with_id {
+    my ($node) = @_;
+
+    my @id_nodes;
+    if (my @child = child_element_nodes($node)) {
+        push(@id_nodes, grep($_->hasAttribute('id'), @child));
+        push(@id_nodes, nodes_with_id($_))     # recursive
+          for @child;
+    }
+    else {
+        return;
+    }
+
+    return(@id_nodes);
 }
 
 sub header_file_id {
@@ -57,21 +88,68 @@ sub child_element_nodes {
 }
 
 sub process_top_nodes {
-    my ($nodes) = @_;
+    my ($nodes, $nodes_by_id) = @_;
 
     # Pass through all the top-level nodes to gather data,
     # since they can refer to other top-level nodes, even those later in the file.
     # { Function => { "_308" => { name => "rd_kafka_version", ... }, ... }, ... }
-    my $nodes_by_name = get_node_data($nodes);
-    #print Dumper($nodes_by_name);
+    my $nodes_by_name = node_data($nodes);
 
-    # Now tie the data together.
-
+    link_data_together($nodes_by_name, $nodes_by_id);
 
 }
 
-sub get_node_data {
+sub link_data_together {
+    my ($nodes_by_name, $nodes_by_id) = @_;
+
+    link_structs($nodes_by_name, $nodes_by_id);   # deletes Field
+    print Dumper($nodes_by_name->{Struct});exit;
+}
+
+sub link_structs {
+    my ($nodes_by_name, $nodes_by_id) = @_;
+
+    # Field are elements of Struct
+    my $fields = delete($nodes_by_name->{Field});    # bye!
+    my $structs = $nodes_by_name->{Struct};
+    my $typedefs = $nodes_by_name->{Typedef};
+    my %cached_typedef_by_type;
+    foreach my $struct_id (keys %$structs) {
+        my $struct = $structs->{$struct_id};
+        if ($struct->{incomplete}) {
+            my $typedef = $cached_typedef_by_type{$struct_id};
+            unless ($typedef) {
+                foreach my $typedef_id (keys %$typedefs) {
+                    my $t = $typedefs->{$typedef_id};
+                    if ($t->{type} eq $struct_id) {
+                        $typedef = $cached_typedef_by_type{$struct_id} = $t;
+                    }
+                }
+            }
+            $struct->{typedef} = {
+                id => $typedef->{id},
+                name => $typedef->{name},
+            };
+        }
+
+        my @member_ids = split(' ', $struct->{members});
+        foreach my $member_id (@member_ids) {
+            my $field = $fields->{$member_id}
+              or die("Struct '$struct_id' member '$member_id' not found");
+
+            push @{ $struct->{fields} }, {
+                name => $field->{name},
+
+                # _type lookup
+
+            };
+        }
+    }
+}
+
+sub node_data {
     my ($nodes) = @_;
+    my %nodes_by_name;
 
     foreach my $node (@$nodes) {
         my $node_name = $node->nodeName;
@@ -149,7 +227,7 @@ sub cb_Typedef {
     my ($node) = @_;
     my $id = $node->getAttribute('id');
 
-    my %attr = map +($_ => $node->getAttribute($_)), qw/name type/;
+    my %attr = map +($_ => $node->getAttribute($_)), qw/id name type/;
 
     return($id => \%attr);
 }
